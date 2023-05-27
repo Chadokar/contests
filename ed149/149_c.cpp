9ae8e78be0a27758199ee5a44153c85fb55209b1
Shubham <shubhamchadokar@rhombuz.io>
2023-05-27 12:04:38 +0530

    149_c file created

---
+++ ed149/149_c.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+vector<ll>arr,prefix;
+
+void solve(){
+    string s;
+    cin>>s;
+    int n=s.size();
+}
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+        solve();
+	}
+}
